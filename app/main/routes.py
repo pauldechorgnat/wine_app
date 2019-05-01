455a55a2ce27372d9b0baf912f07335a3a9e3df5
Paul Dechorgnat <pdechorgnat@yahoo.fr>
2019-05-01 22:04:56 +0200

    adding a logo

--- app/main/routes.py
+++ app/main/routes.py
@@ -49,19 +49,26 @@ def index():
                            prev_url=prev_url)
 
 
-@bp.route('/explore')
+# @bp.route('/explore')
+# @login_required
+# def explore():
+#     page = request.args.get('page', 1, type=int)
+#     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+#         page, current_app.config['POSTS_PER_PAGE'], False)
+#     next_url = url_for('main.explore', page=posts.next_num) \
+#         if posts.has_next else None
+#     prev_url = url_for('main.explore', page=posts.prev_num) \
+#         if posts.has_prev else None
+#     return render_template('index.html', title=_('Explore'),
+#                            posts=posts.items, next_url=next_url,
+#                            prev_url=prev_url)
+
+
+@bp.route('/explore', methods=['GET', 'POST'])
 @login_required
 def explore():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.explore', page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('main.explore', page=posts.prev_num) \
-        if posts.has_prev else None
-    return render_template('index.html', title=_('Explore'),
-                           posts=posts.items, next_url=next_url,
-                           prev_url=prev_url)
+    return render_template('explore.html')
+
 
 
 @bp.route('/user/<username>')
